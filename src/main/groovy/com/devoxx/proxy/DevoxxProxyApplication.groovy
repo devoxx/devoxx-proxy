4207a663835178c552948aa3f4cda1d96e0b6fdf
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2016-01-13 08:33:32 -0500

    Initial commit

---
+++ src/main/groovy/com/devoxx/proxy/DevoxxProxyApplication.groovy
@@ -0,0 +1,12 @@
+package com.devoxx.proxy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class DevoxxProxyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run DevoxxProxyApplication, args
+	}
+}
