ca17d361f61b1e1672c2dc12556c7031e4a7cf7c
Sebastien Arbogast <sebastien.arbogast@gmail.com>
2016-08-26 23:17:11 +0200

    Added logging to speaker service

--- src/main/groovy/com/devoxx/proxy/service/SpeakerService.groovy
+++ src/main/groovy/com/devoxx/proxy/service/SpeakerService.groovy
@@ -5,6 +5,8 @@ import com.devoxx.proxy.api.dto.SpeakerListItem
 import com.devoxx.proxy.api.dto.TalkListItem
 import com.devoxx.proxy.domain.Speaker
 import com.devoxx.proxy.repository.SpeakerRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.PageImpl
@@ -16,11 +18,19 @@ import org.springframework.stereotype.Service
  */
 @Service
 class SpeakerService {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     SpeakerRepository speakerRepository
 
     Page<SpeakerListItem> getSpeakers(boolean withVideo, Pageable pageRequest = null) {
-        List<Speaker> speakers = speakerRepository.findAllByOrderByLastNameAsc().findAll {speaker -> speaker.talks?.size() > 0 && speaker.talks.find {talk-> withVideo ? talk.youtubeVideoId != null : true}}
+        log.info("Starting to load speakers from database...")
+
+        List<Speaker> speakers = speakerRepository.findAllByOrderByLastNameAsc()
+
+        log.info("Loaded ${speakers.size()} speakers from database, now filtering...")
+
+        speakers = speakers.findAll {speaker -> speaker.talks?.size() > 0 && speaker.talks.find {talk-> withVideo ? talk.youtubeVideoId != null : true}}
 
         long totalSpeakers = speakers.size()
 
@@ -29,7 +39,7 @@ class SpeakerService {
             speakers = speakers.subList(pageRequest.offset, Math.min(pageRequest.offset + pageRequest.pageSize, speakers.size()))
         }
 
-        return new PageImpl<SpeakerListItem>(speakers.collect { speaker ->
+        def results = speakers.collect { speaker ->
             new SpeakerListItem(
                     uuid: speaker.businessId,
                     firstName: speaker.firstName,
@@ -38,7 +48,11 @@ class SpeakerService {
                     avatarUrl: speaker.avatarUrl,
                     company: speaker.company
             )
-        }, pageRequest, totalSpeakers)
+        }
+
+        log.info("Filtering done. Returning ${results.size()} speakers.")
+
+        return new PageImpl<SpeakerListItem>(results, pageRequest, totalSpeakers)
     }
 
     SpeakerDetail findSpeakerByUuid(String uuid) {
